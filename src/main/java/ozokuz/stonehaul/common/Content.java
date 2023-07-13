bcbeaf0c516aa433772152f562af32d8aa828bce
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-07-13 17:34:06 +0300

    feat: plant fiber

--- src/main/java/ozokuz/stonehaul/common/Content.java
+++ src/main/java/ozokuz/stonehaul/common/Content.java
@@ -1,6 +1,7 @@
 package ozokuz.stonehaul.common;
 
 import com.tterrag.registrate.util.entry.RegistryEntry;
+import net.minecraft.world.item.Item;
 import ozokuz.stonehaul.Stonehaul;
 import ozokuz.stonehaul.common.tools.CrudeAxe;
 import ozokuz.stonehaul.common.tools.CrudeHoe;
@@ -16,9 +17,14 @@ public class Content {
         REGISTRATE.addRawLang("itemGroup." + Stonehaul.MOD_ID, "Stonehaul");
     }
 
+    // Crude Tools
     public static final RegistryEntry<CrudeAxe> CRUDE_AXE = REGISTRATE.item("crude_axe", CrudeAxe::new).lang("Crude Axe").register();
     public static final RegistryEntry<CrudeHoe> CRUDE_HOE = REGISTRATE.item("crude_hoe", CrudeHoe::new).lang("Crude Hoe").register();
     public static final RegistryEntry<CrudeShovel> CRUDE_SHOVEL = REGISTRATE.item("crude_shovel", CrudeShovel::new).lang("Crude Shovel").register();
+
+    // Crafting Resources
+    public static final RegistryEntry<Item> PLANT_FIBER = REGISTRATE.item("plant_fiber", Item::new).lang("Plant Fiber").register();
+    public static final RegistryEntry<Item> PLANT_STRING = REGISTRATE.item("plant_string", Item::new).lang("Plant String").register();
 
     public static void register() {}
 }
